a474a4766a1475d62309d1a2a99563be6de2da3e
Arthuchaut <arthuchaut@gmail.com>
2020-09-21 21:44:11 +0200

    Fixed dependencies missing

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Arthuchaut/pylock",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'click'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
